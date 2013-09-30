a95a12867dedbc53283773ce8e895dcb537f207f
Kostyantyn <kostya.stepanyuk@gmail.com>
2013-09-30 20:50:51 +0300

    Add products#index as a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Gadgets::Application.routes.draw do
+  root 'products#index'
+
   devise_for :users
+
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
